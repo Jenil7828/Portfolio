6ace9eaf28973da9568ccf54e6c73be96ab3bae2
Jenil Rathod <119324150+Jenil7828@users.noreply.github.com>
2025-03-02 00:04:01 +0530

    Update settings.py

--- home/home/settings.py
+++ home/home/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     path.join(BASE_DIR, 'app/static'),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
